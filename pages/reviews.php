1b9856689d5a5c0ef9394d8c4d2999699498e646
Anastasiia Kudriashova <kudriashova.ag@gmail.com>
2025-06-03 18:44:53 +0300

    Reviews ready

--- pages/reviews.php
+++ pages/reviews.php
@@ -24,5 +24,31 @@
 Name      date
 Message
 
-
  -->
+
+<?php
+    $reviews = file_exists("reviews.json") ? array_reverse(json_decode(file_get_contents("reviews.json"), true)) : [];
+    $perPage = 3;
+    $totalPages = ceil(count($reviews) / $perPage);
+    $p = $_GET['p'] ?? 1;
+
+    // $reviews = array_slice($reviews, ($p - 1) * $perPage, $perPage);
+    $reviews = array_chunk($reviews, $perPage)[$p - 1];
+
+    foreach ($reviews as $review): ?>
+        <div class="border p-3 my-3">
+            <?= $review['name'] ?>, <?= date('d.m.Y H:i', $review['time']) ?>
+            <div>
+                <?= $review['message'] ?>
+            </div>
+        </div>
+  <?php endforeach; ?>
+
+  <nav>
+    <ul class="pagination">
+      <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+        <li class="page-item <?= $i == $p ? 'active' : '' ?>"><a class="page-link" href="/reviews?p=<?= $i ?>"><?= $i ?></a></li>
+      <?php endfor; ?>
+    </ul>
+  </nav>
+
